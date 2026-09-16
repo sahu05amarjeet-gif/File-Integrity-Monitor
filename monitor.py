db6a40b8791a1ef3dfe701bc57a6f58003f061ea
sahu05amarjeet-gif <sahu05amarjeet@gmail.com>
2026-09-16 14:28:42 +0530

    Optimized the code to reduce the scanning time taken initially

--- monitor.py
+++ monitor.py
@@ -3,12 +3,13 @@ from pathlib import Path
 import json
 import os
 import sys
+import time
 
 countModifiedFiles = 0
 countUnchangedFiles = 0
 countNewFilesDetected = 0
 countDeletedFiles = 0
-
+skipped_list_count = 0
 filePath = "monitor.json"
 def load_json(): #load the existing dictionaries from the monitor json file
     with open(filePath, "r") as file:
@@ -26,35 +27,50 @@ def printSummary():
     print(f"Unchanged files: {countUnchangedFiles}")
     print(f"New files: {countNewFilesDetected}")
     print(f"Deleted files: {countDeletedFiles}")
+    print(f"Skipped files: {skipped_list_count}")
     print("======================")
 
 userEnterFolderPath = input("Enter the folder path: ")
 new_hashes = {}
+skipped_list = []
 while (userEnterFolderPath == ""): #Checks if the path is empty or not, if yes then again asks for the folder path
     print("Path cannot be empty")
     userEnterFolderPath = input("Enter the folder path: ")
+
+
 def folderScan(userEnterFolderPath):
     new_hashes = {}
+    skipped_list = []
     contentsOfFolder = Path(userEnterFolderPath)
-    if not Path.exists(userEnterFolderPath): #Checks for the path user enters and checks it if it exists or not
+    if not contentsOfFolder.exists(): #Checks for the path user enters and checks it if it exists or not
         return None
+    
     if contentsOfFolder.is_file(): #Checks if the path entered is a file
         print(f"'{userEnterFolderPath}' is not a folder")
         return {} #return to stop the program scanning further, also folderScan() requires dictionary thats why I used {}
     for item in contentsOfFolder.rglob("*"): #This .rglob("*") -> will search for the files and folders in the directories and sub-dir recusively in the given path object
         relative_path = item.relative_to(contentsOfFolder) #give me the relative path of the ITEMS which are starting from the monitored root (contentsOfFolder)
         relativePathStr = str(relative_path)
-        if item.is_file():
+
+        if item.is_file() and item.suffix.lower() == ".mkv" or item.suffix.lower() == ".mp4" or item.suffix.lower() == ".avi" or item.suffix.lower() == ".mov" or item.suffix.lower() == ".wmv":
+            skipped_list.append(relativePathStr)
+            global skipped_list_count
+            skipped_list_count+=1
+        if item.is_file() and item.suffix.lower() != ".mkv" and item.suffix.lower() != ".mp4" and item.suffix.lower() != ".avi" and item.suffix.lower() != ".mov" and item.suffix.lower() != ".wmv":
+            # start = time.time() 
             with open(item, "rb") as file:
                 digest = hashlib.file_digest(file, "sha256")
                 finalHashResult = digest.hexdigest()
                 new_hashes[relativePathStr] = finalHashResult
+            # end = time.time()
+            # print(f"{relativePathStr} took: {end - start:.2f} Seconds")
     if len(new_hashes) == 0: #Checks if the folder exists but it has no files
         print(f"'{userEnterFolderPath}' contains no Files")
         return {}
-    return new_hashes
+    return new_hashes #Always save the return value in the variable.
 
 new_hashes = folderScan(userEnterFolderPath)
+
 if new_hashes is None:
     print("ERROR: Folder path not found")
     sys.exit()
@@ -67,17 +83,20 @@ def comparison(old_hashes, new_hashes): #requires parameters because the func do
                 global countModifiedFiles #to modify the variable inside the function, python will not think it as a new variable
                 countModifiedFiles+=1
             else:
-                print(f"File unchanged: '{key}'")
                 global countUnchangedFiles
                 countUnchangedFiles+=1
+            print(key)
         if key not in old_hashes:
             print(f"New file detected: '{key}'")
             global countNewFilesDetected
             countNewFilesDetected+=1
-                
+
     #Checking if the file is not in the new scanned result, if not then printing it's name
     for key in old_hashes:
-        if key not in new_hashes:
+        if key not in new_hashes and key in skipped_list:
+            print("Skipped file")
+            
+        if key not in new_hashes and key not in skipped_list:
             missingFile = key
             print(f"File deleted: {missingFile}")
             global countDeletedFiles
@@ -86,14 +105,13 @@ def comparison(old_hashes, new_hashes): #requires parameters because the func do
 try:
     if os.path.exists(filePath):
         old_hashes = load_json() 
-        new_hashes = folderScan(userEnterFolderPath)
+        #Mistake: Don't call the scanning (Expensive) function again, causes the performance issue.
         comparison(old_hashes, new_hashes)
         printSummary()
         update_json()
     else:
-        new_hashes = folderScan(userEnterFolderPath)
+        #Mistake: Don't call the scanning (Expensive) function again, causes the performance issue.
         print("No baseline found\nInitial baseline created successfully\nRun the program again to detect the changes")
-
         update_json()
 
 except FileNotFoundError:   
@@ -106,4 +124,7 @@ except json.JSONDecodeError:
 except NotADirectoryError:
     print("Not a folder")
 
-#Note: This program now scans the files inside sub-folders recursively.
+#Note: Video files such as .mpv, .mp4 and .avi etc are currently excluded to reduce the scanning time.
+#They are intentionally skipped, not treated as deleted.
+#A future version may allow users to configure which file types are monitored or view skipped files individually.
+
